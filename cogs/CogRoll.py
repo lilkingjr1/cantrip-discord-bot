b74a287831a268b51cffbe8b4fe3206e30ecd5d7
Scott Fisher <scott.fisher072@myci.csuci.edu>
2023-05-12 10:08:47 +0000

    Merged in feature/S2-CogRoll (pull request #14)
    Feature/S2 CogRoll
    
    * Minor update for CogRoll
    
    -Changed dice_spec to just dice
    - Added a Description on how to properly use the /roll cog
    
    * Regex
    
    - Cogroll will now parse user dice rolls utilizing Regex expression
    _ User can input multiple dice rolls in one command call by inputting dice rolls into a comma separated list
    
    * Minor update
    
    - Added d100 to valid_dice_rolls
    
    * Update CogRoll.py
    
    - Changes suggested by Sinjin Serrano tested and implemented
    
    
    Approved-by: Sinjin Serrano

--- cogs/CogRoll.py
+++ cogs/CogRoll.py
@@ -1,12 +1,13 @@
 """CogRoll.py
 
 Handles all commands associated with dice rolls and attacks.
-Date: 04/25/2023
+Date: 05/11/2023
 Authors: Scott Fisher, David Wolfe
 Licensed under GNU GPLv3 - See LICENSE for more details.
 """
 
 import random
+import re
 
 import discord
 from discord.ext import commands
@@ -15,8 +16,9 @@ from discord.ext import commands
 class CogRoll(discord.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.valid_dice_types = [4, 6, 8, 10, 12, 20, 100]
+        self.roll_pattern = re.compile(r'(?P<num_dice>\d*)\s*d\s*(?P<dice_type>\d+)\s*(?P<modifier>[+-]\s*\d+)?')
     
-
     @commands.Cog.listener()
     async def on_ready(self):
         """Listener: On Cog Ready
@@ -25,43 +27,54 @@ class CogRoll(discord.Cog):
         """
         print(f"{self.bot.get_datetime_str()}: [Roll] Successfully cached!")
     
-
-    @discord.slash_command(name='roll', description='Rolls 1 or more of the following dice: [d4, d6, d8, d10, d12, d20]')
-    async def roll_dice(self, ctx, dice_spec: str):
-        # Parse the dice specification string (e.g. "2d6+2" or "2d6-2") into number_of_dice, dice_type, and modifier variables
-        try:
-            dice_parts = dice_spec.split('d')
-            number_of_dice = int(dice_parts[0])
-            if '+' in dice_parts[1]:
-                dice_type, modifier = map(int, dice_parts[1].split('+'))
-                operator = '+'
-            elif '-' in dice_parts[1]:
-                dice_type, modifier = map(int, dice_parts[1].split('-'))
-                operator = '-'
-            else:
-                dice_type = int(dice_parts[1])
-                modifier = 0
-                operator = '+'
-        except (ValueError, IndexError):
-            await ctx.respond('Invalid dice specification. Usage: /roll <number of dice>d<dice type>[+/-<modifier>]', ephemeral=True)
-            return
+    def roll_dice(self, num_dice, dice_type, modifier):
+        """Rolls the specified number of dice with the given type and modifier.
         
-        # Roll the dice and compute total
-        if dice_type in [4, 6, 8, 10, 12, 20]:
-            dice_rolls = [random.randint(1, dice_type) for _ in range(number_of_dice)]
-            total = sum(dice_rolls)
-            if operator == '+':
-                total += modifier
-            else:
-                total -= modifier
-            modifier_str = f'{operator}{modifier}' if operator == '-' or modifier >= 0 else f'+{modifier}'
-            await ctx.respond(f"You rolled {number_of_dice}d{dice_type}{modifier_str}: {dice_rolls} {operator} {modifier} = {total}")
-        else:
-            await ctx.respond('Invalid dice type. Available types: 4, 6, 8, 10, 12, 20', ephemeral=True)
+        Returns the list of dice rolls and the total sum.
+        """
+        dice_rolls = [random.randint(1, dice_type) for _ in range(num_dice)]
+        total = sum(dice_rolls)
+        if modifier:
+            total += modifier
+        return dice_rolls, total
+        
+    @discord.slash_command(name='roll', description='Rolls 1 or more of the following dice: [d4, d6, d8, d10, d12, d20, d100]')
+    async def roll_dice_command(self, ctx, dice: discord.Option(str, description="Example Roll: (1d6+2, 3d 10 - 3)", max_length=255, required=True)):
+        # Split the input by comma to separate multiple dice specifications
+        dice_specs = dice.split(',')
+        results = []
+        
+        for dice_spec in dice_specs:
+            # Parse the dice specification string using regex
+            match = self.roll_pattern.match(dice_spec.strip())
+            if not match:
+                results.append('Invalid dice specification! Example Roll: (1d6+2, 3d 10 - 3)')
+                continue
+            
+            # Extract number of dice, dice type, and modifier from regex match object
+            num_dice = 1
+            if (match.group('num_dice') != ''):
+                num_dice = int(match.group('num_dice'))
+                dice_type = int(match.group('dice_type'))
+                modifier_str = match.group('modifier')
+                modifier = int(modifier_str.replace(' ', '')) if modifier_str else 0
+            
+            # Check if dice type is valid
+            if dice_type not in self.valid_dice_types:
+                results.append('Invalid dice type! Valid Dice types: [4, 6, 8, 10, 12, 20]')
+                continue
+            
+            # Roll the dice and compute total
+            dice_rolls, total = self.roll_dice(num_dice, dice_type, modifier)
+            modifier_str = f'{modifier_str.strip()}' if modifier_str else ''
+            result = f"You rolled {num_dice}d{dice_type}{modifier_str}: {dice_rolls} {'+' if modifier >= 0 else '-'} {abs(modifier)} = {total}"
+            results.append(result)
+        
+        await ctx.respond('\n'.join(results))
 
 def setup(bot):
     """Called by Pycord to setup the cog"""
     cog = CogRoll(bot)
     cog.guild_ids = [bot.guild_id]
     bot.add_cog(cog)
-          
+    
